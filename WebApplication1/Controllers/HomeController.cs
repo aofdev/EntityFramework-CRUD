5417c7de259ec3349e2a2de69798b231a2e63a46
aofdev <aof.dev@gmail.com>
2017-09-04 23:06:21 +0700

    first code

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public string Hello()
+        {
+            return "Hello Word";
+        }
+
+
+        public int AddInteger(int a = 0 ,int b = 0)
+        {
+            return a + b;
+        }
+    }
+}
